783d9ff4d7cf074fd20cb5eac0b85f6dd44c22a9
ellyvandertogt <100711482+ellyvandertogt@users.noreply.github.com>
2022-12-16 14:54:57 +0100

    second commit

--- script.js
+++ script.js
@@ -6,14 +6,52 @@
 // } else if (Number(age) === 18) {
 //     alert("Congratulations on your first year of driving. Enjoy the ride!")
 // }
+////////////////////////////////
+// var age = prompt("What is your age?");
+// if (age) {
+//     if (age < 18) {
+//         alert("Sorry, you are too young to drive. Powering off");
+//     } else if (age > 18) {
+//         alert("Powering on. Enjoy the drive!");
+//     } else if (age === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride");
+//     }
+// }
 
-var age = prompt("What is your age?");
-if (age) {
-    if (age < 18) {
-        alert("Sorry, you are too young to drive. Powering off");
-    } else if (age > 18) {
-        alert("Powering on. Enjoy the drive!");
-    } else if (age === 18) {
-        alert("Congratulations on your first year of driving. Enjoy the ride!");
-    }
+//////////////////
+//function declaration
+// function hello() {
+//     console.log("hello");
+// } 
+// hello();
+
+//function expression 
+// let hello = function() {
+// console.log("hello");
+// }
+
+// hello();
+
+// let hello = () => {
+//     console.log("hello");
+// }
+
+// hello();
+
+///////////////////
+
+// const person = "Elly";
+
+// function declaration() {
+//     console.log("declaration");
+// }
+
+// declaration();
+
+const functionExpression = function() {
+    console.log("expression")
 }
+functionExpression();
+
+
+ 
